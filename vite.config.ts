cc7cc03052dc8282b5fa20d904ed47b58ebca035
José Carlos <zecarlosnetodantas@gmail.com>
2026-06-27 00:23:40 -0300

    Comment out base URL in vite config
    Comment out the base URL configuration in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/react-swc' // ou o plugin que estiver usando
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BDay_NasaPhoto/', // 👈 ADICIONE ISSO AQUI
+  // base: '/BDay_NasaPhoto/', // 👈 ADICIONE ISSO AQUI
 })
