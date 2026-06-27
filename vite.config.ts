20e41b29d4c1c8566af43cc095344cc3393a5ad7
José Carlos <zecarlosnetodantas@gmail.com>
2026-06-27 00:02:51 -0300

    Change React plugin and set base path in Vite config
    Updated Vite configuration to use @vitejs/react-swc plugin and added base path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-swc' // ou o plugin que estiver usando
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/BDay_NasaPhoto/', // 👈 ADICIONE ISSO AQUI
 })
